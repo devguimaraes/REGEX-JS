4b4c1f1ebde0fdaaa4edb179726e98e35a6539fe
Bruno Guimarães <devgmrs@gmail.com>
2022-06-06 11:20:10 -0300

    :wrench: Configuração do Prettier e EsLint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["airbnb-base", "prettier"],
+	plugins: ["prettier"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly",
+	},
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {
+		"prettier/prettier": "error",
+
+		"import/prefer-default-export": "off",
+		"react/state-in-constructor": "off",
+		"react/static-property-placement": "off",
+		"react/jsx-props-no-spreading": "off",
+		"react/prop-types": "off",
+		"no-param-reassign": "off",
+		"no-console": "off",
+		camelcase: "off",
+		"class-methods-use-this": "off",
+	},
+};
